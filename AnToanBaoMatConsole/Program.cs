3ae1aee8a4af54713340c0f6a01696e056848ef1
daty.danghuynh <daty.danghuynh20497@gmail.com>
2018-05-04 11:44:08 +0700

    Playfair and Ceasar algorithm

--- AnToanBaoMatConsole/Program.cs
+++ AnToanBaoMatConsole/Program.cs
@@ -10,6 +10,60 @@ namespace AnToanBaoMatConsole
     {
         static void Main(string[] args)
         {
+            #region Ceasar
+
+            //Console.Write("Type a string to encrypt: ");
+            //string userInput = Console.ReadLine();
+
+            //Console.Write("\n");
+
+            //Console.Write("Enter your key: ");
+            //int key = int.Parse(Console.ReadLine());
+
+            //Console.Write("\n");
+
+            //Console.WriteLine("Encrypted Data");
+            //string cipherText = Ceasar.Encipher(userInput, key);
+            //Console.WriteLine(cipherText);
+
+            //Console.Write("\n");
+
+            //Console.WriteLine("Decrypted Data:");
+
+            //string plainText = Ceasar.Decipher(cipherText, key);
+            //Console.WriteLine(plainText);
+
+            //Console.Write("\n");
+
+            #endregion Ceasar
+
+            #region Playfair
+
+            string originalText = "dang thi truc phuong";
+            Console.WriteLine(originalText);
+            string plainText = Playfair.Prepare(originalText);
+            Console.WriteLine(plainText);
+            string key = "playfirexmbcdghjknostuvwz";
+            string cipherText = Playfair.Encipher(key, plainText);
+            Console.WriteLine(cipherText);
+            plainText = Playfair.Decipher(key, cipherText);
+            Console.WriteLine(plainText);
+            Console.WriteLine();
+
+
+            originalText = "Hide the gold in the tree stump.";
+            Console.WriteLine(originalText);
+            plainText = Playfair.Prepare(originalText);
+            Console.WriteLine(plainText);
+            key = "playfirexmbcdghjknostuvwz";
+            cipherText = Playfair.Encipher(key, plainText);
+            Console.WriteLine(cipherText);
+            plainText = Playfair.Decipher(key, cipherText);
+            Console.WriteLine(plainText);
+
+            #endregion Playfair
+
+            Console.ReadKey();
         }
     }
 }
